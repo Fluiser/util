01c387e712e302bc4749b66dcf4401da4fb56e1f
Alexandr <45771430+Fluiser@users.noreply.github.com>
2021-04-30 19:02:46 +0300

    Update Vector2.hpp

--- Vector2.hpp
+++ Vector2.hpp
@@ -11,7 +11,7 @@
 #elif 199711L < VERSION
 #  define VERSION_CPP 11
 #else
-#  error "Vector3.hpp needs at least c++ standrt version 11"
+#  define VERSION_CPP 0
 #endif
 
 #if VERSION_CPP == 14
@@ -30,28 +30,28 @@ namespace math {
                 x(_x), y(_y) {}
 
         //Values +-*/
-        constexpr v2 operator+(const T &value) const noexcept {
+        __constexpr__ v2 operator+(const T &value) const noexcept {
             return v2{
                     x + value,
                     y + value
             };
         }
 
-        constexpr v2 operator-(const T &value) const noexcept {
+        __constexpr__ v2 operator-(const T &value) const noexcept {
             return v2{
                     x - value,
                     y - value
             };
         }
 
-        constexpr v2 operator*(const T &value) const noexcept {
+        __constexpr__ v2 operator*(const T &value) const noexcept {
             return v2{
                     x * value,
                     y * value
             };
         }
 
-        constexpr v2 operator/(const T &value) const noexcept {
+        __constexpr__ v2 operator/(const T &value) const noexcept {
         	if(value == 0) return v2{0, 0};
             return v2{
                     x / value,
@@ -60,28 +60,28 @@ namespace math {
         }
 
         //Vectors +-*/
-        constexpr v2 operator+(const v2 &value) const noexcept {
+        __constexpr__ v2 operator+(const v2 &value) const noexcept {
             return v2{
                     this->x + value.x,
                     this->y + value.y
             };
         }
 
-        constexpr v2 operator-(const v2 &value) const noexcept {
+        __constexpr__ v2 operator-(const v2 &value) const noexcept {
             return v2{
                     this->x - value.x,
                     this->y - value.y
             };
         }
 
-        constexpr v2 operator*(const v2 &value) const noexcept {
+        __constexpr__ v2 operator*(const v2 &value) const noexcept {
             return v2{
                     this->x * value.x,
                     this->y * value.y
             };
         }
 
-        constexpr v2 operator/(const v2 &value) const noexcept {
+        __constexpr__ v2 operator/(const v2 &value) const noexcept {
             return v2{
                     (value.x == 0 ? 0 : this->x / value.x),
                     (value.y == 0 ? 0 : this->y / value.y)
@@ -89,25 +89,25 @@ namespace math {
         }
 
         //Values operator s=
-        __constexpr__ v2 &operator+=(const T &value) noexcept {
+        v2& operator+=(const T &value) noexcept {
             this->x += value;
             this->y += value;
             return *this;
         }
 
-        __constexpr__ v2 &operator-=(const T &value) noexcept {
+        v2& operator-=(const T &value) noexcept {
             this->x -= value;
             this->y -= value;
             return *this;
         }
 
-        __constexpr__ v2 &operator*=(const T &value) noexcept {
+        v2& operator*=(const T &value) noexcept {
             this->x *= value;
             this->y *= value;
             return *this;
         }
 
-        __constexpr__ v2 &operator/=(const T &value) noexcept {
+        v2& operator/=(const T &value) noexcept {
             if(value != 0)
             {
 				this->x /= value;
@@ -121,25 +121,25 @@ namespace math {
         }
 
         //Vec2 operators s=
-        __constexpr__ v2 &operator+=(const v2 &value) noexcept {
+        __constexpr__ v2& operator+=(const v2 &value) noexcept {
             this->x += value.y;
             this->y += value.x;
             return *this;
         }
 
-        __constexpr__ v2 &operator-=(const v2 &value) noexcept {
+        v2& operator-=(const v2 &value) noexcept {
             this->x -= value.y;
             this->y -= value.x;
             return *this;
         }
 
-        __constexpr__ v2 &operator*=(const v2 &value) noexcept {
+        v2& operator*=(const v2 &value) noexcept {
             this->x *= value.y;
             this->y *= value.x;
             return *this;
         }
 
-        __constexpr__ v2 &operator/=(const v2 &value) noexcept {
+        v2& operator/=(const v2 &value) noexcept {
             if(value.y != 0)
             	this->x /= value.y;
             else
